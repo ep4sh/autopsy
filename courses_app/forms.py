93f4bff81e03b92fe29d09d96cf46d2c3c2d286c
Pasha Radchenko <ep4sh2k@mail.com>
2021-03-13 01:07:37 +0300

    Update register/login forms

--- courses_app/forms.py
+++ courses_app/forms.py
@@ -2,14 +2,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+
 class RegistrationForm(FlaskForm):
-    email = StringField('Email',
+    email = StringField('E-mail',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password',
-                           validators=[DataRequired(), Length(min=8, max=20)])
+                             validators=[DataRequired(),
+                                         Length(min=3, max=20)])
     confirm_password = PasswordField('Confirm Password',
-                           validators=[DataRequired(), Length(min=8, max=20),
-                                      EqualTo('password')])
+                                      validators=[DataRequired(),
+                                                  Length(min=3, max=20),
+                                                  EqualTo('password')])
     submit = SubmitField('Sign up')
 
 class LoginForm(FlaskForm):
--- courses_app/routes.py
+++ courses_app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, flash, redirect, url_for
 from courses_app import app
 from courses_app.forms import RegistrationForm, LoginForm
 
@@ -9,28 +9,22 @@ def dashboard():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
-        return do_the_login()
-    else:
-        return show_the_login()
-
-def do_the_login():
-    pass
-
-def show_the_login():
     form = LoginForm()
-    return render_template('login.html', form=form)
+    if request.method == 'POST':
+        if form.validate_on_submit():
+            return redirect(url_for('dashboard'))
+        else:
+            flash(f"Login failed - check the credentials", 'danger')
+            return render_template('login.html', form=form)
+    else:
+        return render_template('login.html', form=form)
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    if request.method == 'POST':
-        return do_the_reg()
-    else:
-        return show_the_reg()
-
-def do_the_reg():
-    pass
-
-def show_the_reg():
     form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"An account {form.email.data} has been created", 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
