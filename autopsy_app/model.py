1268e92f52a70f6920baaf8eea2bae6f52f57c62
Pasha Radchenko <ep4sh2k@mail.com>
2021-03-22 00:49:40 +0300

    Add db migrations

--- autopsy_app/model.py
+++ autopsy_app/model.py
@@ -1,9 +1,11 @@
 from datetime import datetime
+from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from autopsy_app import app, login_manager
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 @login_manager.user_loader
