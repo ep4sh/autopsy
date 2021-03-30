764bdfdfc1dae83642a9db740c50d5aa9a7d6fd0
Pasha Radchenko <ep4sh2k@mail.com>
2021-03-31 00:06:11 +0300

    Add configs based on Dotenv kv

--- autopsy_app/__init__.py
+++ autopsy_app/__init__.py
@@ -7,16 +7,26 @@ from flask_mail import Mail
 
 app = Flask(__name__)
 load_dotenv()
+
+# securty settings
 app.secret_key = os.getenv("FLASK_SECRET")
+# mail settings
+app.mail_server = os.getenv("MAIL_SERVER")
+app.mail_port = os.getenv("MAIL_PORT")
+app.mail_use_ssl = os.getenv("MAIL_USE_SSL")
 app.mail_username = os.getenv("MAIL_USERNAME")
 app.mail_password = os.getenv("MAIL_PASSWORD")
+# database settings
+app.db_uri = os.getenv("SQLALCHEMY_DATABASE_URI")
+app.db_track_modifications = os.getenv("SQLALCHEMY_TRACK_MODIFICATIONS")
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///autopsy.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+# Flask config
+app.config['SQLALCHEMY_DATABASE_URI'] = app.db_uri
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = app.db_track_modifications
 
-app.config['MAIL_SERVER'] = 'smtp.yandex.ru'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_SERVER'] = app.mail_server
+app.config['MAIL_PORT'] = app.mail_port
+app.config['MAIL_USE_SSL'] = app.mail_use_ssl
 app.config['MAIL_USERNAME'] = app.mail_username
 app.config['MAIL_PASSWORD'] = app.mail_password
 
