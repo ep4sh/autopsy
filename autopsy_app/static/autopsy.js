c74797cc3cef4191d25e63871b4914140cffe8f0
Pasha Radchenko <ep4sh2k@mail.com>
2021-04-05 01:40:38 +0300

    Add timestamp! button for Postmortems

--- autopsy_app/static/autopsy.js
+++ autopsy_app/static/autopsy.js
@@ -1,16 +1,23 @@
 function setAva(name) {
     const avatar_name = name + '.png';
-    console.log(avatar_name);
     document.getElementById('avatar').value = avatar_name;
-
     highlight_avatar(name);
     return true;
 }
 
 function highlight_avatar(name) {
-    var x = document.getElementsByClassName("ava")
+    let x = document.getElementsByClassName("ava")
     for (var i = 0; i < x.length; i++) {
        x[i].style.border = "0rem solid white";
     }
     document.getElementById(name).style.border = "0.2rem solid black" ;
+}
+
+function timeStamp() {
+    let current = new Date();
+    let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+    let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+    let dateTime = '\n'+cDate + 'T' + cTime+'\n';
+    document.getElementById('mortem').value += dateTime;
+    console.log(dateTime);
 }
